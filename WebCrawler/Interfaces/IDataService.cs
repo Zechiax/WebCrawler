26c47011386db346eb492c62a6e4b0a01e40a49e
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-05-26 20:32:25 +0200

    add IDataService

---
+++ WebCrawler/Interfaces/IDataService.cs
@@ -0,0 +1,26 @@
+﻿using WebCrawler.Models;
+
+namespace WebCrawler.Interfaces;
+
+public interface IDataService
+{
+    public ICollection<WebsiteRecord> GetWebsiteRecords();
+    
+    public WebsiteRecord GetWebsiteRecord(int id);
+    
+    public void AddWebsiteRecord(WebsiteRecord websiteRecord);
+    
+    public void UpdateWebsiteRecord(WebsiteRecord websiteRecord);
+    
+    public void DeleteWebsiteRecord(int id);
+    
+    public ICollection<Tag> GetTags();
+    
+    public Execution GetExecution(int id);
+    
+    public void AddExecution(Execution execution);
+    
+    public void UpdateExecution(Execution execution);
+    
+    public void DeleteExecution(int id);
+}
