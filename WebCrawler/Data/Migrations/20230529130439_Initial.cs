cb464c22e70de4c6d785f0088761ee89553f068b
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-05-29 15:06:06 +0200

    add initial migration

---
+++ WebCrawler/Data/Migrations/20230529130439_Initial.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebCrawler.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Executions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AdjacencyListJson = table.Column<string>(type: "TEXT", nullable: false),
+                    Started = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Finished = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Executions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WebsiteRecord",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Url = table.Column<string>(type: "TEXT", nullable: false),
+                    Regex = table.Column<string>(type: "TEXT", nullable: false),
+                    Periodicity = table.Column<TimeSpan>(type: "TEXT", nullable: false),
+                    Label = table.Column<string>(type: "TEXT", nullable: false),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastExecutionId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebsiteRecord", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WebsiteRecord_Executions_LastExecutionId",
+                        column: x => x.LastExecutionId,
+                        principalTable: "Executions",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tag",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    WebsiteRecordId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tag", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tag_WebsiteRecord_WebsiteRecordId",
+                        column: x => x.WebsiteRecordId,
+                        principalTable: "WebsiteRecord",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tag_WebsiteRecordId",
+                table: "Tag",
+                column: "WebsiteRecordId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebsiteRecord_LastExecutionId",
+                table: "WebsiteRecord",
+                column: "LastExecutionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tag");
+
+            migrationBuilder.DropTable(
+                name: "WebsiteRecord");
+
+            migrationBuilder.DropTable(
+                name: "Executions");
+        }
+    }
+}
