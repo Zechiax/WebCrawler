e3cec88ebf7166f79c0dd9a4c32cb9eba45d05f4
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-06-04 19:53:32 +0200

    make logging to file asynchronous

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddSwaggerGen(options =>
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .WriteTo.Console()
-    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+    .WriteTo.Async(a => a.File("logs/log.txt", rollingInterval: RollingInterval.Day))
     .CreateLogger();
 
 builder.Logging.ClearProviders();
