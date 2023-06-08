1ab2a9823756ceaaad4b1935d5a208077d08b02e
Josef Lukášek <pepalukasek@gmail.com>
2023-06-08 16:18:44 +0200

    integrating exampl of paginated view

--- WebCrawler/ClientApp/src/AppRoutes.js
+++ WebCrawler/ClientApp/src/AppRoutes.js
@@ -1,6 +1,7 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { Records } from "./components/PaginatedView";
 
 const AppRoutes = [
   {
@@ -14,6 +15,10 @@ const AppRoutes = [
   {
     path: '/fetch-data',
     element: <FetchData />
+  },
+  {
+     path: '/overview',
+      element: <Records />
   }
 ];
 
