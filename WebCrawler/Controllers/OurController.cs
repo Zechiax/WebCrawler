9cb1c142a23fdcd627edc51d72167a4e74f8f204
Tomas Tillmann <tomas.tilllmann@gmail.com>
2023-06-14 11:20:00 +0200

    little refactor

---
+++ WebCrawler/Controllers/OurController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCrawler.Controllers;
+
+public abstract class OurController : Controller
+{
+    public static readonly int InternalErrorCode = 500;
+    public static readonly int NotImplementedCode = 501;
+}
