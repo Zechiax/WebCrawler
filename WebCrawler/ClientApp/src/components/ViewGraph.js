8f465664a23f8a5a0ffcd869ceb9b671b9a3a167
Tomas Tillmann <tomas.tilllmann@gmail.com>
2023-06-17 20:31:31 +0200

    fixed warnings locally - hopefuly on git too

--- WebCrawler/ClientApp/src/components/ViewGraph.js
+++ WebCrawler/ClientApp/src/components/ViewGraph.js
@@ -265,11 +265,11 @@ class ViewGraphInternal extends Component {
   //https://stackoverflow.com/questions/3426404/create-a-hexadecimal-colour-based-on-a-string-with-javascript
   static stringToColour(str) {
     var hash = 0;
-    for (var i = 0; i < str.length; i++) {
+    for (let i = 0; i < str.length; i++) {
       hash = str.charCodeAt(i) + ((hash << 5) - hash);
     }
     var colour = "#";
-    for (var i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
       var value = (hash >> (i * 8)) & 0xff;
       colour += ("00" + value.toString(16)).substr(-2);
     }
