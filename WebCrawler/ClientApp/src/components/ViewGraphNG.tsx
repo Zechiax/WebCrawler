ccbc4fa3fd2984c88aef97cd554f5c40be1f2448
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-06-25 11:45:41 +0200

    fix eslint complaining

--- WebCrawler/ClientApp/src/components/ViewGraphNG.tsx
+++ WebCrawler/ClientApp/src/components/ViewGraphNG.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { DataSet, Network } from 'vis-network/standalone/esm/vis-network';
 import './ViewGraphNG.css';
-import { ProgressBar } from 'react-bootstrap';
+//import { ProgressBar } from 'react-bootstrap';
 
 
 interface INode {
