7310cd7c3acae1bfc831c55a6c4b8e54cc56686f
Tomas Tillmann <tomas.tilllmann@gmail.com>
2023-07-08 17:02:25 +0200

    add node info component

--- WebCrawler/ClientApp/src/components/Home.js
+++ WebCrawler/ClientApp/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
-import { CreateWebsiteRecordModalWindow } from "./CreateWebsiteRecordModalWindow";
+import { WebsiteRecordInfoModal } from "./WebsiteRecordInfoModal";
 import PaginatedView from "./PaginatedView";
 
 export class Home extends Component {
@@ -11,74 +11,77 @@ export class Home extends Component {
     super(props);
 
     this.state = {
-        isCreateWebsiteRecordModalShown: false,
-        dataUpdatehandler: null,
-        modalWindowContext: {
-            id: 0,
-            name: "",
-            isActive: true,
-            tags: [],
-            periodicity: 10,
-            regexPattern: ".*",
-            entryUrl: "",
-            isEditing: false
-        }
+      isCreateWebsiteRecordModalShown: false,
+      dataUpdatehandler: null,
+      modalWindowContext: {
+        id: 0,
+        name: "",
+        isActive: true,
+        tags: [],
+        periodicity: 10,
+        regexPattern: ".*",
+        entryUrl: "",
+        isEditing: false,
+      },
     };
   }
 
-    onCreateWebsiteRecordModalClose = () => {
-        this.setState({ isCreateWebsiteRecordModalShown: false });
-    }
+  onCreateWebsiteRecordModalClose = () => {
+    this.setState({ isCreateWebsiteRecordModalShown: false });
+  };
 
-    handleEditWebsiteRecord = (context) => {
-        this.setState({
-            isCreateWebsiteRecordModalShown: true,
-            modalWindowContext: {
-                ...context,
-                tags: [...context.tags],
-            }
-        });
-    }
+  handleEditWebsiteRecord = (context) => {
+    this.setState({
+      isCreateWebsiteRecordModalShown: true,
+      modalWindowContext: {
+        ...context,
+        tags: [...context.tags],
+      },
+    });
+  };
 
-    registerDataUpdateHandler = (handler) => {
-        this.setState({ dataUpdatehandler: handler });
-    }
+  registerDataUpdateHandler = (handler) => {
+    this.setState({ dataUpdatehandler: handler });
+  };
 
   render() {
     return (
       <>
-        <Container fluid="md" style={{ display: 'flex', flexDirection: 'column'}}>
-            <div style={{ flex: '1', marginBottom: '1rem' }}>
-                    <PaginatedView
-                        showEditModalWindow={this.handleEditWebsiteRecord}
-                        registerDataUpdateHandler={this.registerDataUpdateHandler}
-                    />
-            </div>
+        <Container
+          fluid="md"
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <div style={{ flex: "1", marginBottom: "1rem" }}>
+            <PaginatedView
+              showEditModalWindow={this.handleEditWebsiteRecord}
+              registerDataUpdateHandler={this.registerDataUpdateHandler}
+            />
+          </div>
 
-            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <Button
-                    variant="primary"
-                    onClick={() =>
-                        this.setState({ isCreateWebsiteRecordModalShown: true })
-                    }
-                >
-                    Create new website record
-                </Button>
-            </div>
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Button
+              variant="primary"
+              onClick={() =>
+                this.setState({ isCreateWebsiteRecordModalShown: true })
+              }
+            >
+              Create new website record
+            </Button>
+          </div>
         </Container>
 
-        <CreateWebsiteRecordModalWindow
-            show={this.state.isCreateWebsiteRecordModalShown}
-            urlPresetValue={this.state.modalWindowContext.entryUrl}
-            labelPresetValue={this.state.modalWindowContext.name}
-            isActivePresetValue={this.state.modalWindowContext.isActive}
-            tagsPresetValue={[...this.state.modalWindowContext.tags]}
-            periodicityPresetValue={this.state.modalWindowContext.periodicity}
-            regexPresetValue={this.state.modalWindowContext.regexPattern}
-            recordId={this.state.modalWindowContext.id}
-            isEditing={this.state.modalWindowContext.isEditing}
-            passCreatedRecordId={ this.state.dataUpdatehandler }
-            onClose={this.onCreateWebsiteRecordModalClose}
+        <WebsiteRecordInfoModal
+          show={this.state.isCreateWebsiteRecordModalShown}
+          urlPresetValue={this.state.modalWindowContext.entryUrl}
+          labelPresetValue={this.state.modalWindowContext.name}
+          isActivePresetValue={this.state.modalWindowContext.isActive}
+          tagsPresetValue={[...this.state.modalWindowContext.tags]}
+          periodicityPresetValue={this.state.modalWindowContext.periodicity}
+          regexPresetValue={this.state.modalWindowContext.regexPattern}
+          recordId={this.state.modalWindowContext.id}
+          isEditing={this.state.modalWindowContext.isEditing}
+          passCreatedRecordId={this.state.dataUpdatehandler}
+          onClose={this.onCreateWebsiteRecordModalClose}
         />
       </>
     );
--- WebCrawler/ClientApp/src/components/NodeInfo.js
+++ WebCrawler/ClientApp/src/components/NodeInfo.js
@@ -1,20 +1,92 @@
+import { FaceRetouchingOffRounded } from "@mui/icons-material";
 import React, { Component } from "react";
+import Modal from "react-bootstrap/Modal";
+import ListGroup from "react-bootstrap/ListGroup";
+import { Label } from "reactstrap";
+import Button from "react-bootstrap/Button";
+import { WebsiteRecordInfoModal } from "./WebsiteRecordInfoModal";
 
 export class NodeInfo extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      createNewWebsiteRecord: {
+        show: false,
+      },
+    };
   }
 
   render() {
     return (
-      <ul>
-        {this.props.nodeInfos.map((info) => {
-          <li>
-            {info.label}: {info.value}
-          </li>;
-        })}
-        ;
-      </ul>
+      <>
+        <Modal
+          show={this.props.show}
+          onHide={this.props.onHide}
+          size="lg"
+          centered
+        >
+          <Modal.Header closeButton>Node Info</Modal.Header>
+          <ListGroup
+            style={{
+              margin: 10,
+            }}
+          >
+            <ListGroup.Item>title: {this.props.title}</ListGroup.Item>
+            <ListGroup.Item>url: {this.props.url}</ListGroup.Item>
+            <ListGroup.Item>crawl time: {this.props.crawlTime}</ListGroup.Item>
+            <Label
+              style={{
+                marginTop: "20px",
+                marginLeft: "16px",
+              }}
+            >
+              Website records
+            </Label>
+            <ListGroup>
+              {this.props.records.map((record, i) => {
+                return (
+                  // TODO: add arrow that will reveal more info about website record - component from home.js table
+                  // HOW: we have id, so just fetch the record from server and bind it to the props
+                  <ListGroup.Item
+                    key={i}
+                    variant="light"
+                  >{`${record.label}(${record.id})`}</ListGroup.Item>
+                );
+              })}
+            </ListGroup>
+          </ListGroup>
+          <div
+            style={{
+              margin: 10,
+            }}
+          >
+            <Button
+              onClick={() => {
+                this.setState({
+                  createNewWebsiteRecord: {
+                    show: true,
+                  },
+                });
+              }}
+              variant="primary"
+            >
+              Create new website record from this node
+            </Button>
+          </div>
+        </Modal>
+
+        <WebsiteRecordInfoModal
+          show={this.state.createNewWebsiteRecord.show}
+          onClose={() =>
+            this.setState({
+              createNewWebsiteRecord: {
+                show: false,
+              },
+            })
+          }
+        />
+      </>
     );
   }
 }
